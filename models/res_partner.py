6f52786e28a9d4b6f991e1586f802e934356459a
Grandis SYF <gra@dr.com>
2025-03-09 16:30:50 +0700

    update

--- models/res_partner.py
+++ models/res_partner.py
@@ -1,20 +1,33 @@
 from odoo import models, fields, api
 
+class AccountAccount(models.Model):
+    _inherit = 'account.account'
+
+    company_id = fields.Many2one(
+        'res.company', 
+        string='Company',
+        index=True,
+        readonly=False
+    )
+
 class ResPartner(models.Model):
     _inherit = 'res.partner'
+
+    def _get_simple_domain(self):
+        return [('deprecated', '=', False)]
 
     property_account_payable_id = fields.Many2one(
         'account.account',
         company_dependent=True,
         string="Account Payable",
-        domain="[('deprecated', '=', False), ('company_id', '=', current_company_id)]",
+        domain=lambda self: self._get_simple_domain(),
     )
     
     property_account_receivable_id = fields.Many2one(
         'account.account',
         company_dependent=True,
         string="Account Receivable",
-        domain="[('deprecated', '=', False), ('company_id', '=', current_company_id)]",
+        domain=lambda self: self._get_simple_domain(),
     )
 
     @api.model
